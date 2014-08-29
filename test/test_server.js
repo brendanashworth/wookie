ac4f4156cfa78fa20a99cf09d95ad579bf592282
boboman13 <rich@safe-node.com>
2014-08-29 00:42:33 -0700

    Adds unit testing framework

---
+++ test/test_server.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('wookie http server', function() {
+	describe('wookie http parser', function() {
+		it('should give 200 on GET / request', function() {
+			assert.ok(true, 'perfect');
+		});
+	});
+});
