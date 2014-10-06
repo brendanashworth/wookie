6bfb6407251e72ad95bdc42ebfb9e27e29522d3b
Brendan Ashworth <squirrelslikeacorns@gmail.com>
2014-10-05 19:34:33 -0700

    Try this out

--- src/http_response.c
+++ src/http_response.c
@@ -6,10 +6,52 @@ struct wookie_response {
 	char *content;
 };
 
-void http_response_send(wookie_response *response, int connfd) {
-	//char *format = "HTTP/1.1 %i http response message here\r\nContent-Length: %lu\r\n\r\n%s\r\n";
-	char *message = w_malloc(strlen(format));
-	sprintf(message, "HTTP/1.1 %i HTTP Message here\r\nContent-Length: %lu\r\n\r\n%s\r\n", response->code, strlen(response->content), response->content);
+/**
+ * End a header (or entire HTTP header) of a response.
+ * @param connfd Connection file descriptor.
+ */
+static inline void http_response_end_header(int connfd) {
+	send(connfd, "\r\n", 2, 0);
+}
 
-	send(connfd, message, strlen(message), 0);
+void http_response_send(wookie_response *response, int connfd) {
+	// First send response status
+	http_response_send_statusline("HTTP/1.1", "200", "OK", connfd);
+
+	http_response_send_header("Content-Length", "xyz", connfd);
+	http_response_send_header("Server", "wookie", connfd);
+	http_response_end_header(connfd);
+
+	// Response
+	send(connfd, response->content, strlen(response->content), 0);
+}
+
+void http_response_send_statusline(char *version, char *code, char *message, int connfd) {
+	// Calculate the length of the status line
+	size_t length = strlen(version) + 1 + strlen(code) + 1 + strlen(message) + 2 + 1;
+
+	// Allocate and build the status line
+	char *statusline = w_malloc(length);
+	strcpy(statusline, version);
+	strcat(statusline, " ");
+	strcat(statusline, code);
+	strcat(statusline, " ");
+	strcat(statusline, message);
+	strcat(statusline, "\r\n");
+
+	// Send the status line
+	send(connfd, statusline, length, 0);
+}
+
+void http_response_send_header(char *key, char *value, int connfd) {
+	char *header = w_malloc(strlen(key) + 2 + strlen(value) + 1);
+	strcpy(header, key);
+	strcat(header, ": ");
+	strcat(header, value);
+
+	// Send actual header
+	send(connfd, header, strlen(header), 0);
+
+	// Send line endings
+	http_response_end_header(connfd);
 }
--- src/http_response.h
+++ src/http_response.h
@@ -11,5 +11,22 @@ typedef struct wookie_response wookie_response;
  */
 void http_response_send(wookie_response *response, int connfd);
 
+/**
+ * Sends an HTTP response header to a client.
+ * @param key    The key of the header.
+ * @param value  The value of the header.
+ * @param connfd Connection file descriptor of the client.
+ */
+void http_response_send_header(char *key, char *value, int connfd);
+
+/**
+ * Send an HTTP response status line to a client.
+ * @param version The HTTP version.
+ * @param code    The response status code.
+ * @param message The response status code's message.
+ * @param connfd  The client's connection file descriptor.
+ */
+void http_response_send_statusline(char *version, char *code, char *message, int connfd);
+
 #include "http_response.c"
 #endif
